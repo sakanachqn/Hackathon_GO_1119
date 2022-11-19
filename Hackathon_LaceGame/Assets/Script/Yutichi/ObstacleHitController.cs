0991f7e999f89c44c7c81ff8333cf13381d48a5f
a <aaa>
2022-11-19 18:47:52 +0900

    1119当たり判定の制作(未完)

---
+++ Hackathon_LaceGame/Assets/Script/Yutichi/ObstacleHitController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleHitController : MonoBehaviour
+{
+    // “–‚½‚Á‚½‚ÉŒÄ‚Î‚ê‚éŠÖ”
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Hit"); // ƒƒO‚ğ•\¦‚·‚é
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
